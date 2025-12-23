a8cea8bf562eabcb4ed2c3d8927089314ce6887d
Pc <abdujabborovabdurasul16@gmail.com>
2025-12-23 16:53:54 +0500

    Trying new

---
+++ Practicing.py
@@ -0,0 +1,6 @@
+#today (23.12.2025) I studied how to open files in python. That's actually not that hard, let's try
+with open("pythonpractice", "r") as file:
+    print(file.read())
+    print(file.read(16))
+    print(file.read(5))
+    print(file.read(9))
